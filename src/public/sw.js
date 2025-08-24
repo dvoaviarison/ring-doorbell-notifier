fe6eb3dba08ef1941a77433ebcd35f0554fd5135
Daniel Voaviarison <daniel.voaviarison@gmail.com>
2025-08-24 19:03:21 -0400

    Make it a PWA

---
+++ src/public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'ring-notifier-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/images/apple-touch-icon.png',
+  '/images/favicon.ico',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
